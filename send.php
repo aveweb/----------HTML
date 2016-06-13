ba4ce8367e9689521e98d5fb18814e0fc5c8b5dc
Feram <feram@feram.co>
2016-06-13 08:25:50 +0000

    Fix typos

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 $frm_name  = "Youname";
-$recepient = "youmail@ya.ru";
+$recipient = "youmail@ya.ru";
 $sitename  = "Название Сайта";
 $subject   = "Новая заявка с сайта \"$sitename\"";
 $name = trim($_POST["name"]);
@@ -13,4 +13,4 @@ E-mail: $email <br>
 Телефон: $phone <br>
 Сообщение: $message
 ";
-mail($recepient, $subject, $message, "From: $frm_name <$email>" . "\r\n" . "Reply-To: $email" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
+mail($recipient, $subject, $message, "From: $frm_name <$email>" . "\r\n" . "Reply-To: $email" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
